14de0268f84591e7f82d08dc26e64bccde1a0258
jesse <mr.bossman075@gmail.com>
2020-11-20 23:42:54 -0500

    bruh

---
+++ libgen.py
@@ -0,0 +1,25 @@
+data_file = open("pins.txt", "r")
+lib_file = open("test.lib", "w")
+pins = dict()
+lib_file.write('EESchema-LIBRARY Version 2.4\nDEF rk3368 U 0 40 Y Y 1 F N\nF0 "U" 0 0 50 H V C CNN\nF1 "rk3368" 0 0 50 H V C CNN\nF2 "" 0 0 50 H I C CNN\nF3 "" 0 0 50 H I C CNN\nDRAW\n')
+for aline in data_file:
+	val = aline.split()
+	if val[1] in pins:
+		pins[val[1]] = pins[val[1]] + (val[0],)
+	else:
+		pins.update({val[1]:(val[0], )})
+pos = 0
+for tuples in pins:
+	for pin in pins[tuples]:
+		if pin == pins[tuples][0]:
+			lib_file.write("X " + str(pin) + " " + str(tuples) +" 0 " + str(pos) + " 100 L 50 50 1 1 I\n")
+
+		else:
+			lib_file.write("X " + str(pin) + " " + str(tuples) +" 0 " + str(pos) + " 100 L 50 50 1 1 I N\n")
+	pos += 100
+			
+lib_file.write('ENDDRAW\nENDDEF')
+
+
+
+
